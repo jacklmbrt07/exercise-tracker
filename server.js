50c01d3c6ad939899146dc6c7a21f35eac7e989a
jacklmbrt07 <jacklmbrt07@gmail.com>
2020-12-09 07:00:18 -0800

    can connect to database

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const bodyParser = require('body-parser')
+const bodyParser = require("body-parser");
 const cors = require("cors");
 require("dotenv").config();
 
@@ -17,9 +17,32 @@ mongoose.connect(process.env.MONGO_URI, {
   useUnifiedTopology: true,
 });
 
+let exerciseSessionSchema = new mongoose.Schema({
+  description: { type: String, required: true },
+  duration: { type: Number, required: true },
+  date: String,
+});
+
+let userSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  log: [exerciseSessionSchema],
+});
+
+let Session = mongoose.model("Session", exerciseSessionSchema);
+let User = mongoose.model("User", userSchema);
 
 //Routes
 
+app.post(
+  "/api/exercise/new-user",
+  bodyParser.urlencoded({ extended: false }),
+  (req, res) => {
+    console.log(req.body);
+    res.json({});
+  }
+);
+
+//Listener
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
