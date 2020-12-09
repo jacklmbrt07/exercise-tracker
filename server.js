d05c36d151ccdfe00fecdc7be4d6cdcde2563009
jacklmbrt07 <jacklmbrt07@gmail.com>
2020-12-09 06:29:28 -0800

    second commit

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const bodyParser = require('body-parser')
 const cors = require("cors");
 require("dotenv").config();
 
@@ -8,6 +9,16 @@ app.use(express.static("public"));
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
+
+//Database
+const mongoose = require("mongoose");
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+
+//Routes
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log("Your app is listening on port " + listener.address().port);
